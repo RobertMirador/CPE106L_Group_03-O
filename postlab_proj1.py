4b59d85a11aac4653dfec86ffddcbc8c7a08ba01
Josh Amonoy <87757829+JoshA04@users.noreply.github.com>
2023-03-17 04:48:44 +0400

    Project 1 and Project 2
    Project 1 and Project 2 Python Source Codes Uploaded

---
+++ postlab_proj1.py
@@ -0,0 +1,67 @@
+"""
+File: student.py
+Resources to manage a student's name and test scores.
+"""
+
+class Student(object):
+    """Represents a student."""
+
+    def __init__(self, name, number):
+        """All scores are initially 0."""
+        self.name = name
+        self.scores = []
+        for count in range(number):
+            self.scores.append(0)
+
+    def getName(self):
+        """Returns the student's name."""
+        return self.name
+  
+    def setScore(self, i, score):
+        """Resets the ith score, counting from 1."""
+        self.scores[i - 1] = score
+
+    def getScore(self, i):
+        """Returns the ith score, counting from 1."""
+        return self.scores[i - 1]
+   
+    def getAverage(self):
+        """Returns the average score."""
+        return sum(self.scores) / len(self._scores)
+    
+    def getHighScore(self):
+        """Returns the highest score."""
+        return max(self.scores)
+ 
+    def __str__(self):
+        """Returns the string representation of the student."""
+        return "Name: " + self.name  + "\nScores: " + \
+               " ".join(map(str, self.scores))
+
+    def __eq__(self, student):
+        return self.name == student.name
+    
+    def __lt__(self, student):
+        return self.name < student.name
+    
+    def __ge__(self, student):
+        return self.name >= student.name
+
+def main():
+    """A simple test."""
+    student = Student("Ken", 5)
+    print(student)
+    for i in range(1, 6):
+        student.setScore(i, 100)
+    print(student)
+
+    student2 = Student("Ken", 5)
+    student3 = Student("Annabelle", 5)
+    student4 = Student("Martin", 5)
+
+    print(student.__ge__(student4))
+
+if __name__ == "__main__":
+    main()
+
+

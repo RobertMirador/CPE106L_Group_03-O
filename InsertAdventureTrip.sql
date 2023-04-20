f80a0426a9a8d3fd675e557814c754c63079da2b
RobertMirador <126091488+RobertMirador@users.noreply.github.com>
2023-04-20 14:04:50 +0300

    Add files via upload

---
+++ InsertAdventureTrip.sql
@@ -0,0 +1,22 @@
+INSERT INTO ADVENTURE_TRIP 
+	(
+		TRIP_ID,
+		TRIP_NAME,
+		START_LOCATION,
+		STATE,
+		DISTANCE,
+		MAX_GRP_SIZE,
+		TYPE,
+		SEASON
+	)
+VALUES
+	(
+		45,
+		'Jay Peak',
+		'Jay',
+		'VT',
+		8,
+		8,
+		'Hiking',
+		'Summer'
+	)
